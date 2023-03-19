42f8c97a079b3ea5a8a5a08e1f1bc0dd8b06d0ec
nachog00 <ignaciog00@gmail.com>
2023-03-18 21:47:59 -0300

    Squashed commit of the following:
    commit 3bf82114aec9fe08b5726d0f164230844eb193e3
    Author: nachog00 <ignaciog00@gmail.com>
    Date:   Sat Mar 18 21:46:55 2023 -0300
    
        added index.css again, i found out tailwind is regular css compatible.
    
    commit f30394d6857b2b0639014e069c76b54816331bd9
    Author: nachog00 <ignaciog00@gmail.com>
    Date:   Sat Mar 18 20:27:05 2023 -0300
    
        This version supports tailwind!

--- src/app.jsx
+++ src/app.jsx
@@ -13,25 +13,16 @@ export default function App() {
   // run the function
 
   return (
-    <>
+    <div className="p-10">
         {/* Replace all of this with your app */}
-      <h1>Hello World</h1>
-        <p>My first React app!</p>
-        <div className="fetchedData" style={{
-              width:'20rem',
-              height: '15rem',
-              backgroundColor: 'lightBlue',
-              padding: '1rem',
-              borderRadius: '1rem',
-              border: 'darkBlue 1px solid',
-              fontWeight: 'bold',
-              color: 'darkBlue',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}>
+      <h1 className="text-xl bold">Hello World!</h1>
+        <p className="m-0 text-2xl">My first React app!</p>
+        <div className="fetchedData m-10 w-80 h-60 bg-lightBlue p-4 rounded-lg border border-darkBlue font-bold text-darkBlue flex items-center justify-center">
           <TestRunGoogleScript />
         </div>
-    </>
+
+        <p className="text-xl tc-g">This version of the app includes Tailwind!</p>
+
+    </div>
   );
 }
