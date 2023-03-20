6606b6a159b946fcc1798476a51efd9f66262986
nachog00 <ignaciog00@gmail.com>
2023-03-20 01:39:14 -0300

    added better htmlOutput options.

--- app/server/doGet.js
+++ app/server/doGet.js
@@ -2,11 +2,24 @@
 // it is called by the doGet function in the webapp
 
 const doGet = (e) => {
-    console.log('Opening App: <YourAppName>')
+
+    let options = {
+        appName: '<YourAppName>', // add your app name here
+        favicon: '', // add your favicon url here
+    }
+
+    console.log(`Opening App: ${options.appName}`)
     try{
         // add the path to the directory containing your compiled app
-        let path = '';
-        return HtmlService.createTemplateFromFile(path + 'client/index.html')
+        let path = 'apps/webapp/app/';
+        return HtmlService
+            .createHtmlOutputFromFile(path + 'client/index.html')
+            .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+            .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+            .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+            .setFaviconUrl(options.favicon)
+            .setTitle(options.appName)
+
     } catch (error) {
         console.log(error)
         return HtmlService.createHtmlOutput(error.stack)
